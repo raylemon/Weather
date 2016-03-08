4300c922552f76e79e5a63bc2dd75856b18866f2
raylemon <big0424@gmail.com>
2016-03-08 17:22:00 +0100

    Adapter and aktivity modifications

--- app/src/main/java/com/github/raylemon/weather/ui/MainAktivity.kt
+++ app/src/main/java/com/github/raylemon/weather/ui/MainAktivity.kt
@@ -3,23 +3,32 @@ package com.github.raylemon.weather.ui
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import com.github.raylemon.weather.R
+import com.github.raylemon.weather.data.JsonServer
 import com.github.raylemon.weather.ui.adapter.ForecastAdapter
 import kotlinx.android.synthetic.main.forecast_list.*
+import org.jetbrains.anko.async
+import org.jetbrains.anko.uiThread
 
 /**
  * Created by big04 on 06-03-16.
  */
 class MainAktivity : AppCompatActivity() {
 
-    val items = (1..16).map { "item n° $it" }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
     }
 
+    private val cnt = 16
+    private val city = "Wavre"
+
     override fun onResume() {
         super.onResume()
-        vForecastList.adapter = ForecastAdapter(items)
+        async() {
+            val items = JsonServer().getForecast(cnt, city)
+            uiThread {
+                vForecastList.adapter = ForecastAdapter(items)
+            }
+        }
     }
 }
--- app/src/main/java/com/github/raylemon/weather/ui/adapter/ForecastAdapter.kt
+++ app/src/main/java/com/github/raylemon/weather/ui/adapter/ForecastAdapter.kt
@@ -1,25 +1,36 @@
 package com.github.raylemon.weather.ui.adapter
 
 import android.support.v7.widget.RecyclerView
+import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import com.github.raylemon.weather.R
+import com.github.raylemon.weather.data.domain.Forecast
+import com.github.raylemon.weather.data.domain.ForecastList
+import com.github.raylemon.weather.ext.inflate
+import com.github.raylemon.weather.ext.load
+import com.github.raylemon.weather.ext.toDate
+import kotlinx.android.synthetic.main.item_list_content.view.*
 
 /**
  * Created by big04 on 06-03-16.
  */
-class ForecastAdapter(val items: List<String>) : RecyclerView.Adapter<ForecastAdapter.ViewHolder>() {
+class ForecastAdapter(val items: ForecastList) : RecyclerView.Adapter<ForecastAdapter.ViewHolder>() {
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        return ViewHolder(TextView(parent.context))
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) = ViewHolder(parent.inflate(R.layout.item_list_content, false))
+
+    override fun getItemCount() = items.size
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) = holder.bindForecast(items[position])
+
+    inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        fun bindForecast(forecast: Forecast) = with(forecast) {
+            with(itemView) {
+                vDate.text = dt.toDate()
+                vDesc.text = desc
+                vMaxTemp.text = temp.max.toString()
+                vMinTemp.text = temp.min.toString()
+                vIcon.load(icon)
+            }
+        }
     }
-
-    override fun getItemCount(): Int {
-        return items.size
-    }
-
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.textView.text = items[position]
-    }
-
-    inner class ViewHolder(val textView: TextView) : RecyclerView.ViewHolder(textView)
 }
