bc7a1d82f79c6d7a2ef9a7b587883767bd40c1fe
Christophe Monniez <christophe.monniez@fccu.be>
2016-03-14 19:28:18 +0100

    Paramétrage des widgets de préférence.

--- app/src/main/java/com/github/raylemon/weather/ui/PreferencesDialog.kt
+++ app/src/main/java/com/github/raylemon/weather/ui/PreferencesDialog.kt
@@ -1,6 +1,13 @@
 package com.github.raylemon.weather.ui
 
+import android.app.Dialog
+import android.os.Bundle
 import android.support.v7.app.AppCompatDialogFragment
+import android.view.LayoutInflater
+import android.widget.NumberPicker
+import com.github.raylemon.weather.R
+import org.jetbrains.anko.AlertDialogBuilder
+import org.jetbrains.anko.find
 
 /**
  * Created by christophe on 14/03/16.
@@ -9,5 +16,24 @@ class PreferencesDialog() : AppCompatDialogFragment() {
     companion object {
         const val CITY_KEY = "City"
         const val CNT_KEY = "cnt"
+    }
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        val view = LayoutInflater.from(activity).inflate(R.layout.pref_dialog, null)
+        //TODO
+        val pick = view.find<NumberPicker>(R.id.vDayPicker).apply {
+            minValue = 1
+            maxValue = 16
+            value = 7
+        }
+        return AlertDialogBuilder(activity).apply {
+            title("Preferences")
+            customView(view)
+            positiveButton("Set !")
+            {
+                //TODO
+            }
+            negativeButton("Cancel") { cancel() }
+        }.builder.create()
     }
 }
