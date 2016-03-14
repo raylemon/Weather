8a4ac399dbb0f9ffbaf7280d93e8cc26fe54f82d
Christophe Monniez <christophe.monniez@fccu.be>
2016-03-14 19:07:56 +0100

    Ajout d'une classe kotlin rétro compatible pour la boîte de dialogue.

---
+++ app/src/main/java/com/github/raylemon/weather/ui/PreferencesDialog.kt
@@ -0,0 +1,13 @@
+package com.github.raylemon.weather.ui
+
+import android.support.v7.app.AppCompatDialogFragment
+
+/**
+ * Created by christophe on 14/03/16.
+ */
+class PreferencesDialog() : AppCompatDialogFragment() {
+    companion object {
+        const val CITY_KEY = "City"
+        const val CNT_KEY = "cnt"
+    }
+}
